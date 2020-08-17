2758bc2c467096ed19be01290821b229daf53495
Samara Grant <samara.amanda01@gmail.com>
2020-08-17 15:44:41 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ require 'json'
 class GetRequester
 
   def initialize
-    @URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    @URL = @URL
   end
 
   def get_response_body
