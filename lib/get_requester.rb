7e0b91da3137e99888639681a61c0e1a9f91ac49
Samara Grant <samara.amanda01@gmail.com>
2020-08-17 15:34:40 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,10 +4,6 @@ require 'net/http'
 class GetRequester(URL)
 
 
-  def initialize
-    @URL = @URL
-  end
-
   def get_response_body
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
